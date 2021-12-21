08d51ff0c041475981930dfd2608427762d57283
Max Said <maxsaid95@gmail.com>
2021-12-21 12:23:55 -0500

    removed signup in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,18 +6,6 @@
           <h1 id="title">curren<span>see</span></h1>
         </b-navbar-item>
       </template>
-      <template slot="end">
-        <b-navbar-item tag="div">
-          <div class="buttons">
-            <a class="button is-primary" href="/signup">
-              <strong>Sign up</strong>
-            </a>
-            <a class="button is-light" href="/login">
-              Log in
-            </a>
-          </div>
-        </b-navbar-item>
-      </template>
     </b-navbar>
   </div>
 </template>
